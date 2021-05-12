d8eec2296ec6ed12d0b78efd0b7ead0f1bf8f1fd
viiiraj07 <58074485+viiiraj07@users.noreply.github.com>
2021-05-12 11:06:16 +0530

    Update navbar

--- navbar.js
+++ navbar.js
@@ -7,3 +7,18 @@ document.querySelector('#fb-btn').addEventListener('click', function() {
     window.location.href = "https://www.facebook.com/Codigo-Plataforma-108719471350683";
 
 });
+
+var lastScrollTop = 0;
+
+document.addEventListener("scroll", function() {
+
+    var st = window.pageYOffset || document.documentElement.scrollTop;
+    if (st > lastScrollTop) {
+        document.querySelector('nav').style.position = "relative";
+        document.querySelector('.hamburger').style.position = "absolute";
+    } else {
+        document.querySelector('nav').style.position = "sticky";
+        document.querySelector('.hamburger').style.position = "fixed";
+    }
+    lastScrollTop = st <= 0 ? 0 : st;
+}, false);
